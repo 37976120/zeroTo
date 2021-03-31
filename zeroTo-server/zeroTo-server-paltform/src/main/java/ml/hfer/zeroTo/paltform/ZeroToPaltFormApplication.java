b984315d03df92cd4ed2146fd41bae22c7d8682e
HSL <hsl@123.com>
2021-03-31 16:45:59 +0800

    1

---
+++ zeroTo-server/zeroTo-server-paltform/src/main/java/ml/hfer/zeroTo/paltform/ZeroToPaltFormApplication.java
@@ -0,0 +1,13 @@
+package ml.hfer.zeroTo.paltform;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroToPaltFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroToPaltFormApplication.class);
+    }
+}
